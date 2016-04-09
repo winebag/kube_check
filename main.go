ebbd16cc21c2ab6859345d7745d1a3ecd4a28946
linzhinan(zen Lin) <linzhinan@huawei.com>
2016-04-09 15:56:23 +0800

    Add Phase check
    Signed-off-by: linzhinan(zen Lin) <linzhinan@huawei.com>

--- main.go
+++ main.go
@@ -16,18 +16,18 @@ import (
 func checkKubePods(c *cli.Context) {
 
 	clientConfig := restclient.Config{}
-	clientConfig.Host = "localhost:8080"
+	clientConfig.Host = "127.0.0.1:8080"
 	clientU, err := unversioned.New(&clientConfig)
 	if err != nil {
 		fmt.Println("new unverioned err!")
 	}
 	pods, err := clientU.Pods("").List(api.ListOptions{})
 	if err != nil {
-		fmt.Println("list pods err!")
+		fmt.Printf("list pods err! err := %v\n", err)
 	}
 	for _, pod := range pods.Items {
 		for _, cond := range pod.Status.Conditions {
-			fmt.Printf("pod.Type := %v, pod.Status := %v", cond.Type, cond.Status)
+			fmt.Printf("pod.Name := %v pod.Type := %v, pod.Status := %v ,pod.Status.Phase := %v\n", pod.Name, cond.Type, cond.Status, pod.Status.Phase)
 		}
 	}
 }
