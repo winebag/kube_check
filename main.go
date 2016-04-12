f3b57e40f5b3224dd9d1f62f3fe27fd463fffda7
linzhinan(zen Lin) <linzhinan@huawei.com>
2016-04-12 14:13:48 +0800

    Add getting obj from watch function
    Signed-off-by: linzhinan(zen Lin) <linzhinan@huawei.com>

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	//"bytes"
 	"fmt"
 	"os"
 
+	//"encoding/json"
 	"github.com/codegangsta/cli"
 	"k8s.io/kubernetes/pkg/api"
 	//"k8s.io/kubernetes/pkg/fields"
@@ -11,6 +13,9 @@ import (
 
 	"k8s.io/kubernetes/pkg/client/restclient"
 	"k8s.io/kubernetes/pkg/client/unversioned"
+	// "k8s.io/kubernetes/pkg/runtime"
+	// "k8s.io/kubernetes/pkg/watch/json"
+	// "k8s.io/kubernetes/pkg/field	s"
 )
 
 func checkKubePods(c *cli.Context) {
@@ -30,6 +35,85 @@ func checkKubePods(c *cli.Context) {
 			fmt.Printf("pod.Name := %v pod.Type := %v, pod.Status := %v ,pod.Status.Phase := %v\n", pod.Name, cond.Type, cond.Status, pod.Status.Phase)
 		}
 	}
+}
+
+func watchPod(c *cli.Context) {
+
+	clientConfig := restclient.Config{}
+	clientConfig.Host = "127.0.0.1:8080"
+	clientU, err := unversioned.New(&clientConfig)
+	if err != nil {
+		fmt.Println("new unverioned err!")
+	}
+
+	/*
+		opts := api.ListOptions{
+			LabelSelector: "",
+			FieldSelector: "name=nginx",
+		}
+	*/
+	/*
+			selector := fields.Set{
+				"metadata.name": "ngnix",
+			}.AsSelector()
+
+		opts := api.ListOptions{FieldSelector: selector}
+	*/
+	w, err := clientU.Pods("").Watch(api.ListOptions{})
+	if err != nil {
+		fmt.Println("get watch interface err")
+	} else {
+		for {
+			event, ok := <-w.ResultChan()
+			if !ok {
+				fmt.Errorf("closed early\n")
+				break
+			} else {
+				fmt.Println("11111")
+				fmt.Println(event.Type)
+				//pp := api.Pod(event.Object)
+				/*a, _ := json.Marshal(event.Object)
+				var ap api.Pod
+				json.Unmarshal(a, &ap)
+				fmt.Println(ap)
+				fmt.Println(ap.GetObjectMeta().GetName())
+				for a, _ := range ap.Spec.Containers {
+					fmt.Println(api.Container(a).Name)
+				}*/
+				a := event.Object.(*api.Pod)
+				fmt.Println(a)
+				fmt.Println(a.GetObjectMeta().GetName())
+				// fmt.Println(a.Spec.Containers.(api.Container))
+				for _, a := range a.Spec.Containers {
+					fmt.Println(a.Name)
+				}
+				// obj.(*api.Pod).Spec)
+				//b, err := runtime.Encode(event, event.Object)
+				//fmt.Println(b)
+				//api.Pod(event.Object)
+				//fmt.Println(event.Object)
+				//bytes.NewBuffer(event.Object)
+				//a := fmt.Sprintf(string(event.Object))
+				//fmt.Println(a)
+				//pp := new(api.Pod)
+				//json.Unmarshal([]byte(a), pp)
+				//fmt.Println(pp.GetName())
+				//fmt.Println(pp.GetUID())
+
+			}
+		}
+	}
+	/*
+		pods, err := clientU.Pods("").List(api.ListOptions{})
+		if err != nil {
+			fmt.Printf("list pods err! err := %v\n", err)
+		}
+		for _, pod := range pods.Items {
+			for _, cond := range pod.Status.Conditions {
+				fmt.Printf("pod.Name := %v pod.Type := %v, pod.Status := %v ,pod.Status.Phase := %v\n", pod.Name, cond.Type, cond.Status, pod.Status.Phase)
+			}
+		}
+	*/
 }
 
 func main() {
@@ -68,6 +152,14 @@ func main() {
 			Usage:   "check pod status",
 			Action: func(c *cli.Context) {
 				checkKubePods(c)
+			},
+		},
+		cli.Command{
+			Name:    "watch",
+			Aliases: []string{"w"},
+			Usage:   "watch the pod",
+			Action: func(c *cli.Context) {
+				watchPod(c)
 			},
 		},
 	}
